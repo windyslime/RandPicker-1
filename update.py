56157567fbb7e4109abcbf1515edfbb0e5773029
Wenxuan Shen <bushigemen114@gmail.com>
2025-05-27 21:54:54 +0800

    chore: 升级版本号

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ if sys.platform == "win32":
 
 REPO = "xuanxuan1231/RandPicker"
 UPDATER_REPO = "xuanxuan1231/RandPicker-Updater"
-APP_VERSION = Version("v1.5.1")
+APP_VERSION = Version("v1.6.0")
 
 if sys.platform == "win32":
     # 获取更新器的版本
